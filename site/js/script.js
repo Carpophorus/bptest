02b3989832d9d161b017b53b642ab712fbddb4c7
Marko Mrkonjić <markomrkonjic9@gmail.com>
2017-01-30 16:36:17 +0100

    Add files via upload

--- site/js/script.js
+++ site/js/script.js
@@ -44,6 +44,8 @@ $(function () {
 			},
 			false
 		);
+		history.pushState({state: 1}, null, null);
+		window.scrollTo(0,0);
 	});
 
 	bp.loadHome = function () {
@@ -55,6 +57,7 @@ $(function () {
 			},
 			false
 		);
+		history.pushState({state: 1}, null, null);
 		window.scrollTo(0,0);
 	};
 
@@ -67,7 +70,7 @@ $(function () {
 			},
 			false
 		);
-		// history.pushState({oldScrollPosition: ..., newState: "register"}, null, null);
+		history.pushState({state: 2}, null, null);
 		window.scrollTo(0,0);
 	};
 
@@ -80,6 +83,7 @@ $(function () {
 			},
 			false
 		);
+		history.pushState({state: 3}, null, null);
 		window.scrollTo(0,0);
 	};
 
@@ -92,6 +96,7 @@ $(function () {
 			},
 			false
 		);
+		history.pushState({state: 4}, null, null);
 		window.scrollTo(0,0);
 	};
 
@@ -104,6 +109,7 @@ $(function () {
 			},
 			false
 		);
+		history.pushState({state: 5}, null, null);
 		window.scrollTo(0,0);
 	};
 
@@ -113,15 +119,19 @@ $(function () {
 		switch(articleNumber) {
 			case 1:
 				articleHtml = researchHtml;
+				history.pushState({state: 1001}, null, null);
 				break;
 			case 2:
 				articleHtml = valuesHtml;
+				history.pushState({state: 1002}, null, null);
 				break;
 			case 3:
 				articleHtml = mwHtml;
+				history.pushState({state: 1003}, null, null);
 				break;
 			case 4:
 				articleHtml = mediaHtml;
+				history.pushState({state: 1004}, null, null);
 				break;
 			default:
 				articleHtml = null;
@@ -135,6 +145,53 @@ $(function () {
 		);
 		window.scrollTo(0,0);
 	}
+
+	document.addEventListener("onpopstate", function (event) {
+		var snp = null;
+		if(event.state) {
+			switch(event.state) {
+				case 1:
+					snp = homeHtml;
+					break;
+				case 2:
+					snp = registerHtml;
+					break;
+				case 3:
+					snp = newsHtml;
+					break;
+				case 4:
+					snp = aboutHtml;
+					break;
+				case 5:
+					snp = contactHtml;
+					break;
+				case 1001:
+					snp = researchHtml;
+					break;
+				case 1002:
+					snp = valuesHtml;
+					break;
+				case 1003:
+					snp = mwHtml;
+					break;
+				case 1004:
+					snp = mediaHtml;
+					break;
+				default:
+					snp = null;
+			}
+		}
+		if(snp) {
+			$ajaxUtils.sendGetRequest(
+				snp,
+				function (responseText) {
+					document.querySelector("#main-content").innerHTML = responseText;
+				},
+				false
+			);
+			window.scrollTo(0,0);
+		}
+	});
 
 	global.$bp = bp;
 })(window);
